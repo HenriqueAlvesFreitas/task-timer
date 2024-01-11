e88475c69dd41af9d8553ec98ebb16a8a650f83c
HenriqueFreitas02 <haf@hotmail.com.br>
2024-01-11 18:58:34 -0300

    Fixed numbers

--- src/Components/StopWatch/Style.js
+++ src/Components/StopWatch/Style.js
@@ -18,6 +18,28 @@ export const MyStopWatch = styled.div`
     margin-bottom: 24px;
     padding: 16px 12px;
     width: 100%;
+    
+    .number {
+      background-color: #5D677C;
+      box-shadow: 2px 2px 4px #2B2B2B inset;
+      height: 3.4rem;
+      width: 3rem;
+      padding: 8px 4px;
+      border-radius: 10px;
+
+        @media screen and (min-width:1280px) {
+          height: 10.8rem;
+          width: 9rem;
+        }
+      }
+
+    .division {
+      height: 4.2rem;
+
+      @media screen and (min-width:1280px) {
+        height: 12.6rem;
+      }
+    }
   }
 
   .titulo {
